adf573d7b08a22db4bf586313382f13ff2374887
Nuchana <nuchana.johnson@gmail.com>
2020-10-18 23:19:38 -0400

    fix apiRoutes

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,7 @@ const app = express();
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
+// Sets up the Express app to handle data parsing/ route middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
