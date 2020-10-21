91a8a0611c70f2b1b22d8b542823fec1ddd41f00
Nuchana <nuchana.johnson@gmail.com>
2020-10-21 00:34:53 -0400

    update API route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,67 +2,62 @@ const fs = require("fs");
 
 
 module.exports = function (app) {
-   
-    app.get("/api/notes", function(req, res) {
+    // Basic route that sends the user first to the AJAX Page
+    app.get("/api/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "/../db/db.json"));
     });
-    
-    app.get("/api/notes/:id", function(req, res) {
+
+    app.get("/api/notes/:id", function (req, res) {
         let savedNotes = JSON.parse(fs.readFileSync("/../db/db.json", "utf8"));
         res.json(savedNotes[Number(req.params.id)]);
     });
 
-    
+    app.post("/api/notes", function (req, res) {
+        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+        // It will do this by sending out the value "true" have a table
+        // req.body is available since we're using the body parsing middleware
+        let saveNotes = JSON.parse(fs.readFileSync("/../db/db.json", "utf8"));
+        let newNote = req.body;
+        let uniqueId = (saveNotes.length).toString();
+        newNote.id = uniqueId;
+        saveNotes.push(newNote);
 
-// ========================================================
+    });
 
-    app.get("/api/notes", (req, res) => {
-        // store
-        // .getNotes()
-       console.log(__dirname)
-        fs.readFile(__dirname + "/../db/db.json", "utf8", function (err, notes) {
-            if (err) {
-                throw err;
-            }
+    app.delete("/api/tables", function (req, res){
+        let saveNotes = JSON.parse(fs.readFileSync("/../db/db.json", "utf8"));
+        let noteId = res.params.id;
+        let newId = 0;
+        console.log(`Deleting note with ID ${noteId}`);
+        savedNotes = savedNotes.filter(currentNote => {
+            return currentNote.id !== noteId;
+        })
 
-            // Parse the JSON string to an object
-            const dbJSON = JSON.parse(notes);
-            res.json(dbJSON); 
-        });
+        for (currentNote of saveNotes) {
+            currentNote.id = newID.toString();
+            newId++;
+
+        }
+        fs.writeFileSync("/../db/db.json", JSON.stringify(saveNotes));
+        res.json(saveNotes);
+
     })
+}
+    // ========================================================
 
-        // API POST Requests
-        // Below code handles when a user submits a form and thus submits data to the server.
-        // In each of the below cases, when a user submits form data (a JSON object)
-        // ...the JSON is pushed to the appropriate JavaScript array
-//         app.get("/api/notes", (req, res) => {
-//             // store
-//             // .getNotes()
-//            console.log(__dirname)
-//             fs.readFile(__dirname + "/../db/db.json", "utf8", function (err, notes) {
-//                 if (err) {
-//                     throw err;
-//                 }
-    
-//                 // Parse the JSON string to an object
-//                 const dbJSON = JSON.parse(notes);
-//                 res.json(dbJSON); 
-//             });
+    // app.get("/api/notes", (req, res) => {
+    //     // store
+    //     // .getNotes()
+    //     console.log(__dirname)
+    //     fs.readFile(__dirname + "/../db/db.json", "utf8", function (err, notes) {
+    //         if (err) {
+    //             throw err;
+    //         }
 
-//         })
+    //         // Parse the JSON string to an object
+    //         const dbJSON = JSON.parse(notes);
+    //         res.json(dbJSON);
+    //     });
+    // });
 
-//         app.post("/api/notes", (req, res) => {
-//             // store
-            
-//             fs.readFile(__dirname + "/../db/db.json", "utf8", function (err, notes) {
-//                 if (err) {
-//                     throw err;
-//                 }
-    
-//                 // Parse the JSON string to an object
-//                 const dbJSON = JSON.parse(notes);
-//                 dbJSON.push(req.body)
-//                 // convert to string writeFile
-//                 res.json(dbJSON); 
-//             });
-//         })
+       
